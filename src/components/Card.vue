da54a2a514b71dc3fc0984505cac4d6554313934
SlackerEva <lesiv_anna@mail.ru>
2022-10-10 17:57:36 +0300

    added logic and conection for mark btn

---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+    <a :href="eyeShadow.link" target="_blank">
+      <img :src="eyeShadow.imgPath" :alt="eyeShadow.name" class="h-full w-full object-cover object-center group-hover:opacity-75" />
+    </a>
+  </div>
+  <h3 class="mt-4 text-sm text-black-900">{{ eyeShadow.name }}</h3>
+  <Bookmark_btn :shadowID="eyeShadow.id" v-if="user"/>
+</template>
+<script>
+import Bookmark_btn from '@/components/UI/bookmarkBtn.vue';
+import { computed } from "vue";
+import store from "@/store/store.js";
+export default {
+  name: "card_box",
+  components: {
+    Bookmark_btn,
+  },
+  props: {
+    eyeShadow: Object
+  },
+  setup() {
+    const user = computed(() => store.state.user);
+    return { user };
+  }
+}
+</script>
--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -2,14 +2,10 @@
   <div class="bg-white">
     <div class="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <h2 class="sr-only">Eye Shadows</h2>
-
       <div class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        <a v-for="eyeShadow in data" :key="eyeShadow.id" :href="eyeShadow.link" class="group items-center" target="_blank">
-          <div class="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-            <img :src="eyeShadow.imgPath" :alt="eyeShadow.name" class="h-full w-full object-cover object-center group-hover:opacity-75" />
-          </div>
-          <h3 class="mt-4 text-sm text-black-900">{{ eyeShadow.name }}</h3>
-        </a>
+        <div v-for="eyeShadow in data" :key="eyeShadow.id" class="group items-center" >
+          <Card :eyeShadow="eyeShadow" />
+        </div>
       </div>
     </div>
   </div>
@@ -17,9 +13,13 @@
 
 <script>
 import { ref } from "vue";
+import Card from "@/components/Card.vue";
 import { supabase } from "@/supabase/init.js";
 export default {
   name: "cards_list",
+  components: {
+    Card,
+  },
   setup() {
     const data = ref(null);
     const dataLoaded = ref(null);
