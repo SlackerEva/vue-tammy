6ba61ec822f241300bb2b75a2ebac7bc1251aefd
SlackerEva <lesiv_anna@mail.ru>
2022-10-10 23:31:14 +0300

    added logic for get random color from marked shadows and logic for show all shadows

--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -2,7 +2,10 @@
   <div class="bg-white">
     <div class="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <h2 class="sr-only">Eye Shadows</h2>
-      <button @click="getMarked">jkghjk</button>
+      <button @click="getMarked">Показать отмеченные </button>
+      <button @click="getData">Показать все </button>
+      <button @click="getUnmarked">Показать не отмеченные </button>
+      <button @click="getRandom">Рандомный цвет </button>
       <div class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         <div v-for="eyeShadow in data" :key="eyeShadow.id" class="group items-center shadow-lg" >
           <Card :eyeShadow="eyeShadow" />
@@ -52,8 +55,35 @@ export default {
       }
     };
 
+    const getUnmarked = async () => {
+      try {
+      //  let { data: markedSh, error } = await supabase.from('userShadows').select(`user, shadow, eyeShadows (*)`).eq('user', user.value);
+        let { data: markedSh, error } = await supabase.from('userShadows').select(`eyeShadows (*)`).eq('user', user.value);
+        if (error) throw error;
+        if (markedSh) {
+          data.value = markedSh.map(i => i.eyeShadows);
+        }
+      } catch (error) {
+        console.warn(error.message);
+      }
+    };
+
+    const getRandom = async () => {
+      try {
+        let { data: markedSh, error } = await supabase.from('userShadows').select(`eyeShadows (*)`).eq('user', user.value);
+        if (error) throw error;
+        if (markedSh) {
+          let leng = markedSh.length;
+          let random = Math.floor(Math.random() * leng);
+          data.value = markedSh[random];
+        }
+      } catch (error) {
+        console.warn(error.message);
+      }
+    };
+
     getData();
-    return { data, dataLoaded, user, getMarked };
+    return { data, dataLoaded, user, getMarked, getData, getUnmarked, getRandom };
   },
 };
 
