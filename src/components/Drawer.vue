c18b298eb1273e618dd669366fec4092a89fd02f
SlackerEva <lesiv_anna@mail.ru>
2022-10-21 01:30:40 +0300

    change pressed to coin

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -79,19 +79,19 @@
             <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
               <div class="flex items-center pl-3">
                 <input 
-                  id="pressed" 
+                  id="coin" 
                   type="checkbox" 
-                  value="pressed"
+                  value="coin"
                   v-model="chkbxChk"
                   class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300
                   focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 
                   focus:ring-2 dark:bg-gray-600 dark:border-gray-500" 
                 >
                 <label 
-                  for="pressed" 
+                  for="coin" 
                   class="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300"
                 >
-                  {{$t('drawer.pressed')}}
+                  {{$t('drawer.coin')}}
                 </label>
               </div>
             </li>
